6f84f599a2261a64a3f5648aff3c4b13883fb36f
kentaroy47 <kentaroy@stanford.edu>
2019-01-31 20:08:54 -0800

    change some student prediction files.

--- script_make_dataset_train.py
+++ script_make_dataset_train.py
@@ -9,8 +9,17 @@ topx = 256
 target = "jackson2"
 
 # make dataset.
+    # make student predictions
+com = "python demo-and-eval-save-student.py --target "+target+" --net res18 --image_dir images/"+target+"_train --coco True --cuda --dataset pascal_voc --checksession 500 --checkepoch 40 --checkpoint 625"
+subprocess.call(com, shell=True)
 
+    # culling by confusion.
 
+    # make teacher predictions.
+    
+    # culling by precision.
+
+# compile difficult dataset.
 
 # train model.
 
