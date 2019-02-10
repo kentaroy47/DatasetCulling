c0892c2ff892866b57551605a5fa634ff2ecc1bb
kentaroy47 <kentaroy@stanford.edu>
2019-02-09 23:46:03 -0800

    pkls..

--- lib/make.sh
+++ lib/make.sh
@@ -12,7 +12,7 @@ export CFLAGS="-std=c99"
 python setup.py build_ext --inplace
 rm -rf build
 
-CUDA_ARCH= -gencode arch=compute_52,code=sm_52
+CUDA_ARCH= -gencode arch=compute_61,code=sm_61
 "-gencode arch=compute_30,code=sm_30 \
            -gencode arch=compute_35,code=sm_35 \
            -gencode arch=compute_50,code=sm_50 \
